55d6f4e6558eb054d5173933387a5d1c0eb189e6
mvanlaar <martijn@mvanlaar.net>
2015-10-31 22:10:46 +0100

    init

---
+++ CI-InselAirImporter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_InselAirImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class CIFLight
+    {
+        // Auto-implemented properties. 
+        public string FromIATA;
+        public string ToIATA;
+        public DateTime FromDate;
+        public DateTime ToDate;
+        public Boolean FlightMonday;
+        public Boolean FlightTuesday;
+        public Boolean FlightWednesday;
+        public Boolean FlightThursday;
+        public Boolean FlightFriday;
+        public Boolean FlightSaterday;
+        public Boolean FlightSunday;
+        public DateTime DepartTime;
+        public DateTime ArrivalTime;
+        public String FlightNumber;
+        public String FlightAirline;
+        public String FlightOperator;
+        public String FlightAircraft;
+        public String FlightDuration;
+        public Boolean FlightCodeShare;
+        public Boolean FlightNextDayArrival;
+        public int FlightNextDays;
+    }
+}
